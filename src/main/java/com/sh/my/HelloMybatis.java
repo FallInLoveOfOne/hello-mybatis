697e492970580100b8cc214f0b76ce65e15a5c12
suh <2220388107@qq.com>
2025-04-30 14:50:28 +0800

    新增xml文件方式构建mybatis

--- src/main/java/com/sh/my/HelloMybatis.java
+++ src/main/java/com/sh/my/HelloMybatis.java
@@ -1,7 +1,10 @@
 package com.sh.my;
 
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
@@ -9,15 +12,17 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
-@Slf4j
-public class Main {
+import lombok.extern.slf4j.Slf4j;
 
-    public static void HelloMybatis() {
+@Slf4j
+public class HelloMybatis {
+
+    public static void Hello() {
         log.info("Hello MyBatis!");
         // 1. 数据源（这里用MyBatis内置简单连接池 PooledDataSource）
         PooledDataSource dataSource = new PooledDataSource();
         dataSource.setDriver("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://127.0.0.01:3506/ttttt?serverTimezone=UTC");
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3506/ttttt?serverTimezone=UTC");
         dataSource.setUsername("root");
         dataSource.setPassword("root");
 
@@ -41,6 +46,21 @@ public class Main {
         L2Cache(sqlSessionFactory);
     }
 
+    public static void HelloWithConfigFile() {
+        log.info("Hello MyBatis with Config!");
+        // 通过配置文件创建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory;
+        try (InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml")) {
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load MyBatis config", e);
+        }
+
+        // 开启Session，执行查询
+        L1Cache(sqlSessionFactory);
+        L2Cache(sqlSessionFactory);
+    }
+
     private static void L1Cache(SqlSessionFactory sqlSessionFactory) {
         log.info("L1Cache=====");
         int id = 1;
@@ -49,14 +69,14 @@ public class Main {
         User user = mapper.selectUserById(id);
         User user1 = mapper.selectUserById(id);
         log.info("user==user1? {}", user == user1);
-        //只要插入的数据，都会刷新缓存
+        // 只要插入的数据，都会刷新缓存
         mapper.insertUser(new User(2, "小红@"));
         User user2 = mapper.selectUserById(id);
         log.info("user==user2? {}", user == user2);
-        //sqlSession.clearCache();
+        // sqlSession.clearCache();
 
-        //一级缓存是sqlSession级别，即同一个SqlSession中的数据是共享的
-        //相同数据放在map中，同一对象
+        // 一级缓存是sqlSession级别，即同一个SqlSession中的数据是共享的
+        // 相同数据放在map中，同一对象
         UserMapper mapper1 = sqlSessionFactory.openSession(true).getMapper(UserMapper.class);
         User user3 = mapper1.selectUserById(id);
         log.info("user==user3? {}", user == user3);
@@ -65,9 +85,9 @@ public class Main {
     private static void L2Cache(SqlSessionFactory sqlSessionFactory) {
         log.info("L2Cache=====");
         int id = 1;
-        //二级缓存是SqlSessionFactory级别，即同一个SqlSessionFactory中的数据是共享的
-        //跨sqlSession，sqlSession关闭不能持有对象引用，所以会通过序列化保存数据状态
-        //会先将对象序列化，然后进行缓存，新的Session查询到的数据，会进行反序列化,所以会有性能上的提升
+        // 二级缓存是SqlSessionFactory级别，即同一个SqlSessionFactory中的数据是共享的
+        // 跨sqlSession，sqlSession关闭不能持有对象引用，所以会通过序列化保存数据状态
+        // 会先将对象序列化，然后进行缓存，新的Session查询到的数据，会进行反序列化,所以会有性能上的提升
         UserMapper mapper = sqlSessionFactory.openSession(true).getMapper(UserMapper.class);
         User user = mapper.selectUserById(id);
         UserMapper mapper2 = sqlSessionFactory.openSession(true).getMapper(UserMapper.class);
@@ -76,8 +96,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // System.out.println("Hello world!");
-        HelloMybatis();
+        Hello();
+        HelloWithConfigFile();
     }
 
 }
